105bf60985f00b79c528c43ff32de487ce7d9148
_MeePooh_ <122891690+PluemDontKnowToCode@users.noreply.github.com>
2026-08-28 02:26:46 +0700

    feat: implement transaction api

--- backend/app/routers/transactions.py
+++ backend/app/routers/transactions.py
@@ -22,8 +22,12 @@ router = APIRouter(prefix="/transactions", tags=["Transactions"])
 fetch("http://localhost:8080/transactions/1/balance").then(r => r.json()).then(console.log)
 ```""")
 def get_balance(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     # TODO: implement get balance
-    return BalanceResponse(username="somchai", money=500.0)
+    return BalanceResponse(username=user.username, money=user.money)
 
 
 @router.post("/{user_id}/deposit", description="""ฝากเงิน
@@ -37,6 +41,22 @@ fetch("http://localhost:8080/transactions/1/deposit", {
 }).then(r => r.json()).then(console.log)
 ```""")
 def deposit(user_id: int, req: DepositWithdrawRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    updated_money = user.money + req.amount
+    newTransaction = Transaction(
+        user_id=user_id,
+        credit_card=mask_credit_card(user.credit_card),
+        old_money=user.money,
+        updated_money=updated_money,
+        transaction_amount=req.amount,
+        status="success",
+    )
+    user.money = updated_money
+    db.add(newTransaction)
+    db.commit()
     # TODO: implement deposit
     return {"message": "deposited", "amount": req.amount}
 
@@ -52,6 +72,28 @@ fetch("http://localhost:8080/transactions/1/withdraw", {
 }).then(r => r.json()).then(console.log)
 ```""")
 def withdraw(user_id: int, req: DepositWithdrawRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user.money < req.amount:
+        raise HTTPException(status_code=400, detail="Insufficient funds")
+
+    updated_money = user.money - req.amount
+
+    if(updated_money < 0):
+        raise HTTPException(status_code=400, detail="Insufficient funds")
+    newTransaction = Transaction(
+        user_id=user_id,
+        credit_card=mask_credit_card(user.credit_card),
+        old_money=user.money,
+        updated_money=updated_money,
+        transaction_amount=req.amount,
+        status="success",
+    )
+    user.money = updated_money
+    db.add(newTransaction)
+    db.commit()
     # TODO: implement withdraw
     return {"message": "withdrawn", "amount": req.amount}
 
@@ -63,5 +105,19 @@ def withdraw(user_id: int, req: DepositWithdrawRequest, db: Session = Depends(ge
 fetch("http://localhost:8080/transactions/1/history").then(r => r.json()).then(console.log)
 ```""")
 def get_transaction_history(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     # TODO: implement get transaction history
-    return []
+    transactions = db.query(Transaction).filter(Transaction.user_id == user_id).all()
+    
+    transaction_responses = [
+        TransactionResponse(
+            credit_card=mask_credit_card(transaction.credit_card),
+            old_money=transaction.old_money,
+            updated_money=transaction.updated_money,
+            transaction_amount=transaction.transaction_amount,
+            status=transaction.status,
+        ) for transaction in transactions]
+    return transaction_responses
